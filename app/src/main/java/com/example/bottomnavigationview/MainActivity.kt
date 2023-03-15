f482ffa2f51e00205d633d9b438f6e2f36ffd8f2
stasvitalich <stasvitalich1994@gmail.com>
2023-03-15 20:44:01 +0600

    Simple onClick logic was added to bottom items

--- app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bottomnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.bottomnavigationview.databinding.ActivityMainBinding
 
 
@@ -13,5 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bNov.selectedItemId = R.id.item1 // This item will be on focus
+        binding.bNov.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.item1 -> {
+                    Toast.makeText(this, "Item 1",Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Item 2",Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Item 3",Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 -> {
+                    Toast.makeText(this, "Item 4",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            true
+        }
     }
+
+
 }
